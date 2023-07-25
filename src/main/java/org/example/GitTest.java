adf90a07f1426aceb5aaa136fb000256b8652465
zezeze <1660851947@qq.com>
2023-07-25 10:59:04 +0800

    second commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class GitTest {
     public static void main(String[] args){
+
+        System.out.println("Hello git!!!");
         System.out.println("Hello git!!!");
     }
 }
