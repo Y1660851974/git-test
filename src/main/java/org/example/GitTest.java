f3845a19bb0f599b05354630584b6f590139beb6
zezeze <1660851947@qq.com>
2023-07-26 10:38:12 +0800

    push commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("Hello git4");
         System.out.println("master test");
         System.out.println("Hot-fix test");
+        System.out.println("push test");
+
     }
 }
